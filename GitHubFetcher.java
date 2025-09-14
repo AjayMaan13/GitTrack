e5c9a76f4381b1f9aeb6e3a4578da14158e6c8ba
AjayMaan13 <Ajaypsmaanm344@gmail.com>
2025-09-14 17:30:56 -0400

    Added Logic between 3 Java files

--- GitHubFetcher.java
+++ GitHubFetcher.java
@@ -7,28 +7,33 @@ import java.net.http.HttpResponse;
 
 public class GitHubFetcher {
     public static String getRequest(String username) throws IOException, InterruptedException {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
 
-        HttpClient client = HttpClient.newHttpClient();
+            String token = TokenManager.getGitHubToken();
 
-        String token = TokenManager.getGitHubToken();
+            HttpRequest.Builder builder = HttpRequest.newBuilder()
+                    .uri(URI.create("https://api.github.com/repos/" + username + "/REPO/issues/events"))
+                    .header("Accept", "application/vnd.github+json")
+                    .header("User-Agent", "mycliapp")
+                    .header("X-GitHub-Api-Version", "2022-11-28");
 
-        HttpRequest.Builder builder = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.github.com/repos/OWNER/REPO/issues/events"))
-                .header("Accept", "application/vnd.github+json")
-                .header("User-Agent", "mycliapp")
-                .header("X-GitHub-Api-Version", "2022-11-28");
+            if (token != null && !token.isEmpty()) {
+                builder.header("Authorization", "Bearer " + token);
+            }
 
-        if (token != null && !token.isEmpty()) {
-            builder.header("Authorization", "Bearer " + token);
+            HttpRequest request = builder.GET().build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println("Status: " + response.statusCode());
+            System.out.println("Body: " + response.body());
+
+            return response.body();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
 
-        HttpRequest request = builder.GET().build();
-
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        System.out.println("Status: " + response.statusCode());
-        System.out.println("Body: " + response.body());
-
-        return response.body();
+        return null;
     }
 }
